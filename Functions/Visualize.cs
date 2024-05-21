64144f8d74ce344c708bd7f184fbe1f8940959b2
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2024-05-21 03:37:36 +0000

    Count for each item for visual

--- Functions/Visualize.cs
+++ Functions/Visualize.cs
@@ -31,11 +31,19 @@ namespace Money.Function
                 using(var cmd = new MySqlCommand())
                 {
                     cmd.Connection = conn;
-                    cmd.CommandText = "SELECT location, category_id, SUM(amount) FROM items GROUP BY location, category_id";
+                    cmd.CommandText = 
+                        "SELECT " +
+                        "  location, category_id, SUM(amount), COUNT(*) " +
+                        "FROM " +
+                        $"  {SystemVariables.TableExpenseItems} " +
+                        "GROUP BY " +
+                        "  location, category_id";
 
                     var rdr = cmd.ExecuteReader();
 
                     var result = new List<VisualizeRecord>();
+
+                    // this adds the categories for the items to leaf off of
                     categories.ForEach(x =>
                     {
                         result.Add(new VisualizeRecord(x.label));
@@ -44,7 +52,8 @@ namespace Money.Function
                     {
                         var category_find = categories.Find(x => x.id == (int)rdr[1]);
                         string category_name = category_find != null ? category_find.label : "error";
-                        result.Add(new VisualizeRecord((string)rdr[0], category_name, (decimal)rdr[2]));
+                        // (int)(long) : this is being used because MySQL is returning a int64
+                        result.Add(new VisualizeRecord((string)rdr[0], category_name, (decimal)rdr[2], (int)(long)rdr[3]));
                     }
                     
                     return new OkObjectResult(result);
@@ -54,22 +63,22 @@ namespace Money.Function
 
         public class VisualizeRecord
         {
-            public string location {get; set;}
-            public string category {get; set;}
-            public decimal amount {get; set;}
+            public string location { get; set; }
+            public string? category { get; set; }
+            public decimal amount { get; set; }
+            public int count { get; set; }
 
-            public VisualizeRecord(string location, string category, decimal amount)
+            public VisualizeRecord(string location, string category, decimal amount, int count)
             {
                 this.location = location;
                 this.category = category;
                 this.amount = amount;
+                this.count = count;
             }
 
             public VisualizeRecord(string category)
             {
                 this.location = category;
-                this.category = "";
-                this. amount = 0;
             }
         }
     }

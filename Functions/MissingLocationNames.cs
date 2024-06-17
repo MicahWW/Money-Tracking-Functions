906b761ee2cdf7b93c4a1f16b4baa3922216be6e
Micah Warren <26724012+MicahWW@users.noreply.github.com>
2024-06-16 21:34:01 -0500

    Location names features (#28)
    * allows the addition of new location names
    
    * add endpoint to find missing location names
    
    * whitespace formatting
    
    * document endpoint

---
+++ Functions/MissingLocationNames.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Money.Tables;
+
+namespace Money.Functions
+{
+    public class MissingLocationNames
+    {
+        private readonly ILogger<MissingLocationNames> _logger;
+
+        public MissingLocationNames(ILogger<MissingLocationNames> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("MissingLocationNames")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "missing-location-names")] HttpRequest req)
+        {
+            return new OkObjectResult(LocationNamesTable.MissingLocationNames());
+        }
+    }
+}

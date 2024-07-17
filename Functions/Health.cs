6907ad2ee005a8b7abb08a63f087f9a6dd44c575
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2024-07-17 02:39:55 +0000

    health probe

---
+++ Functions/Health.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Money.Modules;
+
+namespace Money.Functions
+{
+    public class Health
+    {
+        private readonly ILogger<Health> _logger;
+        private readonly HealthCheckService _healthCheck;
+
+        public Health(ILogger<Health> logger, HealthCheckService healthCheck)
+        {
+            _logger = logger;
+            _healthCheck = healthCheck;
+        }
+
+        [Function("Health")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            var healthStatus = await _healthCheck.CheckHealthAsync();
+            string systemVariablesStatus;
+            if (SystemVariables.CheckAll())
+                systemVariablesStatus = "Healthy";
+            else
+                systemVariablesStatus = "Unhealthy";
+            
+            var output = new Dictionary<dynamic, dynamic>
+            {
+                { "healthStatus", healthStatus.Status.ToString() },
+                { "systemVariablesStatus", systemVariablesStatus }
+            };
+
+            if (systemVariablesStatus == "Healthy" && healthStatus.Status == HealthStatus.Healthy)
+                return new OkObjectResult(output);
+            else
+                return new ErrorResponse(output, 503);
+        }
+    }
+}
